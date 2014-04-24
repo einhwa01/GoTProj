a2f62fa67a3aa0f28b4da7dea8f78088ba2bff7b
Walter Einhorn <waltre8@gmail.com>
2014-04-23 20:54:00 -0400

    initial maven project
    idk

---
+++ sim/src/main/java/got/sim/App.java
@@ -0,0 +1,13 @@
+package got.sim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
